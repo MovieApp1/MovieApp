9002e85284aaa174a8c51c9c37a9f5507f74e824
MonkeyBoy248 <kirik201100@mail.ru>
2022-08-15 00:19:16 +0300

    build: add craco.config.js for path aliasing

---
+++ craco.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@routing': path.resolve(__dirname, 'src/routing'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+};
