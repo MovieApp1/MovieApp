9e94a9a688bd7f80ddf3841fd176be3678b0094e
MonkeyBoy248 <kirik201100@mail.ru>
2022-08-24 00:33:57 +0300

    feat: add a stroke property to Icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import sprite from '../assets/icons-sprite.svg';
+import sprite from '../assets/icons/icons-sprite.svg';
 
 interface IconProps {
   width: string;
   height: string;
   id: string;
   fill?: string;
+  stroke?: string;
   block: string;
 }
 
-export const Icon = function ({ id, width, height, block, fill }: IconProps) {
+export const Icon = function ({ id, width, height, block, fill, stroke }: IconProps) {
   return (
     <svg className={`${block}__${id}-icon`}
          fill={fill ? fill : undefined}
          width={width}
          height={height}
          viewBox='0 0 24 24'
+         stroke = {stroke}
     >
     <use href={`${sprite}#${id}`} />
     </svg>
