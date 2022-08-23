4415e57d67137b6cd557c6bff97dfe140a86b46d
MonkeyBoy248 <kirik201100@mail.ru>
2022-08-24 00:29:24 +0300

    feat: add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './pages/Home';
 import './styles/_global.scss';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Home></Home>
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Slider from "../features/Recommendations/components/Slider/Slider";
+
+const Home = () => {
+  return (
+    <>
+    <Slider></Slider>
+    </>
+  )
+}
+
+export default Home;
