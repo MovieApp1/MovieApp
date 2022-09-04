c198bbfa3fbebc943d2861cc054d1eb8fd1c72dd
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-04 22:41:06 +0300

    feat: add top list components to the Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Slider from "../features/Recommendations/components/Slider/Slider";
+import Header from "@components/Header/Header";
+import Slider from "@features/Recommendations/components/Slider/Slider";
+import { topMovies, topSeries } from "@services/mockService";
+import TopList from "@features/Recommendations/components/TopList/TopList";
 
 const Home = () => {
   return (
-    <>
-    <Slider></Slider>
-    </>
+    <main>
+      <Slider></Slider>
+      <TopList title='Highly rated movies' topList={topMovies} />
+      <TopList title='Highly rated series' topList={topSeries} />
+    </main>
   )
 }
 
