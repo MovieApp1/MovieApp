4fc83cc7cb279c12feb787b407599484205433e2
MonkeyBoy248 <kirik201100@mail.ru>
2022-09-04 22:39:42 +0300

    feat: add mock data

---
+++ src/services/mockService.ts
@@ -0,0 +1,61 @@
+export interface MockMainPageData {
+  title: string;
+  year: number;
+  score: number;
+}
+
+export const topMovies: MockMainPageData[] = [
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    score: 9.2
+  },
+  {
+    title: 'The Godfather',
+    year: 1972,
+    score: 9.2
+  },
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    score: 9.0
+  },
+  {
+    title: 'The Godfather Part II',
+    year: 1974,
+    score: 9.2
+  },
+  {
+    title: '12 Angry Men',
+    year: 1957,
+    score: 8.9
+  },
+];
+
+export const topSeries: MockMainPageData[] = [
+  {
+    title: 'Planet Earth II',
+    year: 2016,
+    score: 9.4
+  },
+  {
+    title: 'Breaking Bad',
+    year: 2008,
+    score: 9.2
+  },
+  {
+    title: 'Planet Earth',
+    year: 2006,
+    score: 9.4
+  },
+  {
+    title: 'Band of Brothers',
+    year: 2001,
+    score: 9.4
+  },
+  {
+    title: 'Chernobyl',
+    year: 2019,
+    score: 9.3
+  },
+]
